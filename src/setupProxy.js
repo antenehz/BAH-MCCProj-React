bf19a824796098c80766912e879521e26b7adde5
Anteneh Zeleke <48229364+antenehz@users.noreply.github.com>
2022-11-01 13:50:28 -0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
 
     app.use(proxy('/api', { target: 'http://10.108.80.136:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.110.121.156:8081/' })); 
+    app.use(proxy('/account', { target: 'http://10.111.194.237:8081/' })); 
 	
 	/*app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.18.0.3:8081/' }));
